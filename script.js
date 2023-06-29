bef937d96c58cfc0f94977b257fdd61a104962da
William-Graves <williamallengraves@gmail.com>
2023-06-28 23:56:28 -0500

    added

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function getResults(){
+fetch(requestUrl)
+ .then (function (response)  {
+    if (response.ok)   {
+        console.log(response);
+        response.JSON().then(function (data)  {
+            console.log(data);
+            displayResults(data, user);
+        });else {}
+        }
+    }
+ }
 
+
+
+}
+https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
